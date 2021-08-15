3900c37d5e63af7e4c07bff37da60fafece6d259
Mandaw <77012627+mandaw2014@users.noreply.github.com>
2021-08-15 16:26:12 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='Mandaw',
+      version='1.0.0',
+      description='A Game Engine Made In Python',
+      author='mandaw2014',
+      author_email='mandawbuisness@gmail.com',
+      url='https://github.com/mandaw2014/MandawEngine',
+      packages=['mandaw'],
+      package_dir={'mandaw':'mandaw'}
+     )
