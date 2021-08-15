1337b1f41b53f0acee9fc47ec0ba354a916faf54
Mandaw <77012627+mandaw2014@users.noreply.github.com>
2021-08-15 19:25:01 +0100

    Add files via upload

---
+++ mandaw/mandaw.py
@@ -0,0 +1,25 @@
+import turtle
+
+from mandaw.gameobject import GameObject
+from mandaw.text import Text
+from mandaw.audio import Audio
+
+class Mandaw:
+    def __init__(self, title, width, height, bgcolor):
+        super().__init__()
+        
+        self.width = width
+        self.height = height
+
+        self.window = turtle.Screen()
+        self.window.title(title)
+        self.window.bgcolor(bgcolor)
+        self.window.setup(width, height)
+        self.window.tracer(0)
+        self.window.listen()
+
+        self.window.onkeypress(turtle.bye, "Escape")
+
+    def run(self):
+        self.tick(60)
+        self.window.update()
