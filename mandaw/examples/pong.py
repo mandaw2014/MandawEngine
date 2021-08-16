ce35cbebb97a7164b2f0efcc08facd25f10729bb
Mandaw <77012627+mandaw2014@users.noreply.github.com>
2021-08-16 20:09:46 +0100

    added classes

--- mandaw/examples/pong.py
+++ mandaw/examples/pong.py
@@ -7,73 +7,94 @@ mandaw = Mandaw("Pong")
 bg_color = Color("black")
 light_gray = (200, 200, 200)
 
-ball = GameObject(mandaw.window, mandaw.width / 2 - 15, mandaw.height / 2 - 15, light_gray, 20, 20)
-player = GameObject(mandaw.window, mandaw.width - 20, mandaw.height / 2 - 70, light_gray, 10, 140) 
-opponent = GameObject(mandaw.window, 10, mandaw.height / 2 - 70, light_gray, 10, 140)
+class Paddle(GameObject):
+    def __init__(self, x, y):
+        super().__init__(
+            mandaw.window,
+            10,
+            140,
+            x,
+            y,
+            light_gray
+        )
 
-ball_speed_x = 7 * random.choice((1, -1))
-ball_speed_y = 7 * random.choice((1, -1))
-player_pos = mandaw.height / 2 - 70
-opponent_speed = 7
+        self.player_pos = mandaw.height / 2 - 70
+        self.opponent_speed = 7
+
+    def player_movement(self):
+        self.y = self.player_pos
+
+        if self.top <= 0:
+            self.top = 0
+        if self.bottom >= mandaw.height:
+            self.bottom = mandaw.height
+
+    def opponent_movement(self):
+        if self.top < ball.y:
+            self.top += self.opponent_speed
+        if self.bottom > ball.y:
+            self.bottom -= self.opponent_speed
+
+        if self.top <= 0:
+            self.top = 0
+        if self.bottom >= mandaw.height:
+            self.bottom = mandaw.height
+
+class Ball(GameObject):
+    def __init__(self):
+        super().__init__(
+            mandaw.window,
+            20,
+            20,
+            mandaw.width / 2 - 15,
+            mandaw.height / 2 - 15,
+            light_gray
+        )
+
+        self.ball_speed_x = 7 * random.choice((1, -1))
+        self.ball_speed_y = 7 * random.choice((1, -1))
+
+    def ball_movement(self):
+        # Animate the ball
+        self.x += self.ball_speed_x
+        self.y += self.ball_speed_y
+
+        # Collisions
+        if self.top <= 0 or self.bottom >= mandaw.height:
+            self.ball_speed_y *= -1
+        if self.left <= 0 or self.right >= mandaw.width:
+            self.ball_reset()
+
+        if self.colliderect(player) or self.colliderect(opponent):
+            self.ball_speed_x *= -1
+
+    def ball_reset(self):
+        self.center = (mandaw.width / 2, mandaw.height / 2)
+        self.ball_speed_y *= random.choice((1, -1))
+        self.ball_speed_x *= random.choice((1, -1))
+
+ball = Ball()
+player = Paddle(mandaw.width - 20, mandaw.height / 2 - 70)
+opponent = Paddle(10, mandaw.height / 2 - 70)
 
 speed = 7
-
-def ball_movement():
-    global ball_speed_x, ball_speed_y
-    # Animate the ball
-    ball.x += ball_speed_x
-    ball.y += ball_speed_y
-
-    # Collisions
-    if ball.top <= 0 or ball.bottom >= mandaw.height:
-        ball_speed_y *= -1
-    if ball.left <= 0 or ball.right >= mandaw.width:
-        ball_reset()
-
-    if ball.colliderect(player) or ball.colliderect(opponent):
-        ball_speed_x *= -1
-
-def player_movement():
-    player.y = player_pos
-
-    if player.top <= 0:
-        player.top = 0
-    if player.bottom >= mandaw.height:
-        player.bottom = mandaw.height
-
-def opponent_movement():
-    if opponent.top < ball.y:
-        opponent.top += opponent_speed
-    if opponent.bottom > ball.y:
-        opponent.bottom -= opponent_speed
-
-    if opponent.top <= 0:
-        opponent.top = 0
-    if opponent.bottom >= mandaw.height:
-        opponent.bottom = mandaw.height
-
-def ball_reset():
-    global ball_speed_x, ball_speed_y
-    ball.center = (mandaw.width / 2, mandaw.height / 2)
-    ball_speed_y *= random.choice((1, -1))
-    ball_speed_x *= random.choice((1, -1))
 
 while True:
     # Handling inputs
     if mandaw.keys[mandaw.UP]:
-        player_pos -= speed
+        player.player_pos -= speed
 
     if mandaw.keys[mandaw.DOWN]:
-        player_pos += speed
+        player.player_pos += speed
 
     # Ball movement
-    ball_movement()
+    ball.ball_movement()
     
     # Player movement
-    player_movement()
+    player.player_movement()
 
     # Opponent movement
-    opponent_movement()
+    opponent.opponent_movement()
 
     # Visuals
     mandaw.window.fill(bg_color)
