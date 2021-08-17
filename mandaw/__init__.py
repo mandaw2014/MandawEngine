057df62c88360de176b6e653485af224b58c23ea
Mandaw <77012627+mandaw2014@users.noreply.github.com>
2021-08-17 11:29:31 +0100

    Delete mandaw directory

--- mandaw/__init__.py
+++
@@ -1,4 +0,0 @@
-from mandaw.main import Mandaw
-from mandaw.gameobject import GameObject
-from mandaw.line import Line
-from mandaw.color import Color
--- mandaw/color.py
+++
@@ -1,5 +0,0 @@
-import pygame
-
-class Color(pygame.Color):
-    def __init__(self, color):
-        self.color = color
