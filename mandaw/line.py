7617095b14fa2ca1f1472b57461d3e846034a360
Mandaw <77012627+mandaw2014@users.noreply.github.com>
2021-08-18 17:27:01 +0100

    Update line.py

--- mandaw/line.py
+++ mandaw/line.py
@@ -1,10 +1,10 @@
 import pygame
 
 class Line:
-    def __init__(self, window, color, width, height):   
+    def __init__(self, window, color, start_pos, end_pos):   
         self.window = window
         self.color = color
-        self.width = width
-        self.height = height
+        self.start_pos = start_pos
+        self.end_pos = end_pos
 
-        pygame.draw.aaline(self.window.window, self.color, self.width, self.height)
+        pygame.draw.aaline(self.window.window, self.color, self.start_pos, self.end_pos)
