44decd8ecb55716f51bfb9dc3b3e394e4f985127
EyuphanMandiraci <mandiracieyuphan@gmail.com>
2021-08-18 16:22:28 +0300

    added text object (running with pygame.font) added pygame as dependency to setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,10 @@ setup(name='Mandaw',
       version='1.0.1',
       description='A Game Engine Made In Python',
       author='mandaw2014',
+      keywords="python 2d game development",
       author_email='mandawbuisness@gmail.com',
       url='https://github.com/mandaw2014/MandawEngine',
       packages=['mandaw'],
-      package_dir={'mandaw':'mandaw'}
-     )
+      package_dir={'mandaw': 'mandaw'},
+      install_requires=["pygame"]
+      )
