eddd807ebe2b6f2f439e377ef4fafb419e95ca85
Mandaw <77012627+mandaw2014@users.noreply.github.com>
2021-08-18 21:12:56 +0100

    added input and audio

---
+++ mandaw/input.py
@@ -0,0 +1,15 @@
+import pygame
+
+class Input:
+    def __init__(self):
+        self.UP = pygame.K_UP
+        self.DOWN = pygame.K_DOWN
+        self.LEFT = pygame.K_LEFT
+        self.RIGHT = pygame.K_RIGHT
+        
+        self.W = pygame.K_w
+        self.A = pygame.K_a
+        self.S = pygame.K_s
+        self.D = pygame.K_d
+
+        self.SPACE = pygame.K_SPACE
