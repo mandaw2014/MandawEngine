8ff968dde21a92c71ba63396e6dc095bfe7ecee6
Mandaw <77012627+mandaw2014@users.noreply.github.com>
2021-08-19 17:13:58 +0100

    added the center() function

--- mandaw/examples/sprite-animation.py
+++ mandaw/examples/sprite-animation.py
@@ -1,23 +1,25 @@
 from mandaw import *
 
-m = Mandaw()
+mandaw = Mandaw("Sprite Animation")
 
-sp = sprite.Sprite(m, "assets/adventurer.png", 0, 0, (200, 200))
-a = animation.Animation("assets/idle1", 0.14)
-run_anim = animation.Animation("assets/normal-run", 0.14)
-sp.add_animation(a, "idle")
-sp.add_animation(run_anim, "run")
+character = Sprite(window = mandaw, image = "assets/adventurer.png", x = 0, y = 0, size = (200, 200))
+character.center()
 
+animation = Animation("assets/idle1", 0.14)
+run_anim = Animation("assets/normal-run", 0.14)
+
+character.add_animation(animation, "idle")
+character.add_animation(run_anim, "run")
 
 while True:
-    sp.draw()
-    if m.controls.is_key_pressed(m.keys["D"]):
-        sp.play_animation("run")
-        sp.x += 5
-    elif m.controls.is_key_pressed(m.keys["A"]):
-        sp.play_animation("run", "x")
-        sp.x -= 5
+    character.draw()
+    if mandaw.controls.is_key_pressed(mandaw.keys["D"]):
+        character.play_animation("run")
+        character.x += 5
+    elif mandaw.controls.is_key_pressed(mandaw.keys["A"]):
+        character.play_animation("run", "x")
+        character.x -= 5
     else:
-        sp.play_animation("idle")
+        character.play_animation("idle")
 
-    m.run()
+    mandaw.run()
