9805e8d0ca309721047ae88f7f629ca2a6fe5ced
Mandaw <77012627+mandaw2014@users.noreply.github.com>
2021-09-04 19:30:58 +0100

    added mouse input to input.py

--- mandaw/input.py
+++ mandaw/input.py
@@ -1,11 +1,16 @@
 import pygame
 
-class Controls:
+class Input:
     def __init__(self):
         self.pressed = pygame.key.get_pressed()
+        self.mouse = pygame.mouse.get_pressed()
 
     def update(self):
         self.pressed = pygame.key.get_pressed()
+        self.mouse = pygame.mouse.get_pressed()
 
-    def is_key_pressed(self, key):
+    def get_key_pressed(self, key):
         return self.pressed[key]
+
+    def get_mouse_button(self, button):
+        return self.mouse[button]
