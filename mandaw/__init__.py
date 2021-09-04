6d316c5a175d3b8c65e7adb0a4107bddbd37ad37
Mandaw <77012627+mandaw2014@users.noreply.github.com>
2021-09-04 19:31:27 +0100

    changed controls name to input

--- mandaw/__init__.py
+++ mandaw/__init__.py
@@ -7,7 +7,7 @@ from mandaw.line import Line
 from mandaw.color import Color
 from mandaw.sprite import Sprite
 from mandaw.text import Text
-from mandaw.input import Controls
+from mandaw.input import Input
 from mandaw.audio import Audio
 from mandaw.animation import Animation
 from mandaw.light import Light
